1376fe6f3ebd5e2bac015a967c9a5b4edd7eb9ce
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2025-11-15 23:00:49 +0600

    work home

--- factroial.php
+++ factroial.php
@@ -6,7 +6,41 @@
     <title>Document</title>
 </head>
 <body>
-    <h3>Factrail number</h3>
+    <h3>Factorial number</h3><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Factorial Checker</title>
+</head>
+<body>
+    <h3>Factorial number</h3>
+    <form action="" method="post">
+        <label>Number:</label>
+        <input type="number" name="myNumber"> <br>
+        <input type="submit" name="submit" value="Check">
+    </form>
+
+    <?php 
+    if(isset($_POST["submit"])){
+        $num = $_POST["myNumber"];
+
+        if($num == 0){
+            echo "Factorial of $num is: 0";
+            exit;
+        }
+
+        $fact = 1;
+        for($i = 1; $i <= $num; $i++){
+            $fact *= $i;
+        }
+
+        echo "Factorial of $num is: $fact";
+    }
+    ?>
+</body>
+</html>
+
     <?php
     if (isset($_REQUEST["submit"])) {
         $mynumber = $_REQUEST["mynumber"];
--- prime_check.php
+++ prime_check.php
@@ -6,34 +6,34 @@
     <title>Document</title>
 </head>
 <body>
+    <h2>Prime checker</h2>
+    <form action="" method="post">
+        <input type="number" name="myNumber"> <br>
+        <input type="submit" name="submit" value="Check">
+    </form>
+
     <?php
-   
-    if (isset($_REQUEST["submit"])) {
-        $mynumber = $_REQUEST["mynumber"];
-        if($mynumber==1){
-            echo "$mumber is neither prime nor composite.";
-        } elseif ($mynumber == 2) {
-            echo "$mynumber is a prime number.";
-        } else {
-            $isPrime = true;
-            for ($i = 2; $i <= ($mynumber); $i++) {
-                if ($mynumber % $i == 0) {
-                    $isPrime = false;
-                    break;
-                }
-            }
-            if ($isPrime) {
-                echo "$mynumber is a prime number.";
-            } else {
-                echo "$mynumber is not a prime number.";
+ if(isset($_REQUEST["submit"])){
+    $myNumber = $_REQUEST["myNumber"];
+    // echo "My number is ".$myNumber."";
+    if($myNumber == 1){
+        echo "$myNumber is not a prime number";
+    } elseif($myNumber == 2){
+        echo "$myNumber is a prime number";
+    } else {
+        $isPrime = true;
+        for($i = 2; $i < $myNumber; $i++){
+            if($myNumber % $i == 0){
+                $isPrime = false;
+                break;
             }
         }
+    } if ($isPrime){
+        echo "$myNumber is prime number";
+    }else{
+        echo "$myNumber is not a prime number";
     }
+ }
     ?>
-        
-    <form action="">
-        <input type="number" name="mynumber" placeholder="Enter your number"> <br>
-        <input type="submit" name="submit" value="CHECK">
-    </form>
 </body>
 </html>
