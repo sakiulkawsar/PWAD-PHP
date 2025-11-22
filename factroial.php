bbe00ab6b4249f5ae58b1b8b90a3c9b1f0adac01
sakiulkawsar <mr.sakiulkawsar@gmail.com>
2025-11-23 00:09:54 +0600

    work at home

--- factroial.php
+++ factroial.php
@@ -3,30 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3>Factorial number</h3><!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Factorial Checker</title>
 </head>
 <body>
-    <h3>Factorial number</h3>
+    <h3>Factorial Number</h3>
+
     <form action="" method="post">
         <label>Number:</label>
-        <input type="number" name="myNumber"> <br>
+        <input type="number" name="mynumber" required> <br><br>
         <input type="submit" name="submit" value="Check">
     </form>
 
     <?php 
     if(isset($_POST["submit"])){
-        $num = $_POST["myNumber"];
 
+        $num = $_POST["mynumber"];
+
+        // factorial of 0 is 1 (not 0)
         if($num == 0){
-            echo "Factorial of $num is: 0";
+            echo "<h3>Factorial of 0 is: 1</h3>";
             exit;
         }
 
@@ -35,27 +30,8 @@
             $fact *= $i;
         }
 
-        echo "Factorial of $num is: $fact";
+        echo "<h3>Factorial of $num is: $fact</h3>";
     }
     ?>
-</body>
-</html>
-
-    <?php
-    if (isset($_REQUEST["submit"])) {
-        $mynumber = $_REQUEST["mynumber"];
-       $factorial = 1;
-        for ($i = 1; $i <= $mynumber; $i++) {
-           
-            $factorial *= $i;
-        }
-        echo "Factorial of $mynumber is: $factorial";
-
-    }
-    ?>
-    <form action="">
-        <input type="number" name="mynumber" placeholder="Enter your number"> <br>
-        <input type="submit" name="submit" value="CHECK">
-    </form>
 </body>
 </html>
